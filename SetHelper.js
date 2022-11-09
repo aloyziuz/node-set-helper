2a0cec157a52dc027328922c8712413ef2a58e76
Aloysius <aloyziuz@users.noreply.github.com>
2022-11-09 17:55:57 +0700

    updated code to es6 module

--- SetHelper.js
+++ SetHelper.js
@@ -1,44 +1,42 @@
-module.exports = {
-    /**
+/**
      * perform a set union from 2 array. returns a set object
      * @param {array} a 
      * @param {array} b 
      */
-    SetUnion: function(a, b){
-        return new Set(a.concat(b));
-    }, 
-    
-    /**
-     * perform set intersection. will return common elements of the 2 array
-     * @param {array} a 
-     * @param {array} b 
-     * @param {Function} transformFn
-     */
-    SetIntersection: function(a, b, transformFn){
-        var setOfB = new Set(b);
-        return new Set(a.filter(function(memberOfA){
-            let compareValue = memberOfA;
-            if(transformFn){
-                compareValue = transformFn(memberOfA);
-            }
-            return setOfB.has(compareValue);
-        }));
-    }, 
+export function SetUnion(a, b){
+    return new Set(a.concat(b));
+}
 
-    /**
-     * perform set difference. will return elements in a that does not exist in b
-     * @param {array} a 
-     * @param {array} b 
-     * @param {Function} transformFn
-     */
-    SetDifference: function(a, b, transformFn){
-        var setOfB = new Set(b);
-        return new Set(a.filter(function(memberOfA){
-            let compareValue = memberOfA;
-            if(transformFn){
-                compareValue = transformFn(memberOfA);
-            }
-            return !setOfB.has(memberOfA);
-        }));
-    }
+/**
+ * perform set intersection. will return common elements of the 2 array
+ * @param {array} a 
+ * @param {array} b 
+ * @param {Function} transformFn
+ */
+export function SetIntersection(a, b, transformFn){
+    var setOfB = new Set(b);
+    return new Set(a.filter(function(memberOfA){
+        let compareValue = memberOfA;
+        if(transformFn){
+            compareValue = transformFn(memberOfA);
+        }
+        return setOfB.has(compareValue);
+    }));
+}
+
+/**
+ * perform set difference. will return elements in a that does not exist in b
+ * @param {array} a 
+ * @param {array} b 
+ * @param {Function} transformFn
+ */
+export function SetDifference(a, b, transformFn){
+    var setOfB = new Set(b);
+    return new Set(a.filter(function(memberOfA){
+        let compareValue = memberOfA;
+        if(transformFn){
+            compareValue = transformFn(memberOfA);
+        }
+        return !setOfB.has(memberOfA);
+    }));
 }
